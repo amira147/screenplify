e715d91088b33d3a9179f1ec71cf20e20108c8a3
Amira <amira@omniphics.com>
2015-12-03 10:37:39 +0800

    Changed logos to 300px width

---
+++ clients.php
@@ -0,0 +1,18 @@
+<ul>
+	<li><div><img src="./assets/img/client_logos/mydin.png"/></div></li>
+	<li><div><img src="./assets/img/client_logos/altel.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/mahsing.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/ioi.jpg" /></div></li>
+	<li><div><img src="./assets/img/client_logos/bv.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/macy.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/sunwayproperty.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/matta.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/chermainepoo.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/tedxkl.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/ges.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/klims.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/homedec.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/itxasia.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/startupmalaysia.png" /></div></li>
+	<li><div><img src="./assets/img/client_logos/tpm.png" /></div></li>
+</ul>
